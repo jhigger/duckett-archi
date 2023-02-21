9cec93750fbedd2d4cf2f1ea04d668fb020d843e
jhigger <njohnjhigger@gmail.com>
2023-02-21 13:43:15 +0800

    edit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,24 +5,53 @@ const Footer = () => {
 		<footer className="mt-8 flex items-center justify-center gap-16 p-8">
 			<Logo />
 			<div className="flex flex-col items-center justify-center gap-4">
-				<p>&copy; Ducktt Architecture</p>
-				<p>All Rights Reserved</p>
+				<p className="font-bold">&copy; Ducktt Architecture</p>
+				<p className="font-bold">All Rights Reserved</p>
 				<p>
 					Web Design by{" "}
-					<a href="#" target="_blank" rel="noopener noreferrer">
+					<a
+						href="#"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="text-primary"
+					>
 						SG Designs
 					</a>
 				</p>
 			</div>
 			<ul className="flex flex-col items-center justify-center gap-2">
-				<li>footer link 1</li>
-				<li>footer link 2</li>
-				<li>footer link 3</li>
-				<li>footer link 4</li>
+				<li>
+					<a href="#" target="_blank" rel="noopener noreferrer">
+						footer link 1
+					</a>
+				</li>
+				<li>
+					<a href="#" target="_blank" rel="noopener noreferrer">
+						footer link 2
+					</a>
+				</li>
+				<li>
+					<a href="#" target="_blank" rel="noopener noreferrer">
+						footer link 3
+					</a>
+				</li>
+				<li>
+					<a href="#" target="_blank" rel="noopener noreferrer">
+						footer link 4
+					</a>
+				</li>
 			</ul>
 			<ul className="flex items-center justify-center gap-4">
-				<li>social 1</li>
-				<li>social 2</li>
+				<li>
+					<a href="#" target="_blank" rel="noopener noreferrer">
+						social link 1
+					</a>
+				</li>
+				<li>
+					<a href="#" target="_blank" rel="noopener noreferrer">
+						social link 2
+					</a>
+				</li>
 			</ul>
 		</footer>
 	);
