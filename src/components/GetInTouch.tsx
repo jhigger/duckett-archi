86b69741388274ec79e166fdb1b3817ebf4c3626
jhigger <njohnjhigger@gmail.com>
2023-02-23 17:57:49 +0800

    edit

--- src/components/GetInTouch.tsx
+++ src/components/GetInTouch.tsx
@@ -2,10 +2,10 @@ import Button from "./Button";
 
 const GetInTouch = () => {
 	return (
-		<div className="flex flex-col items-center justify-center gap-8 py-16">
+		<section className="flex flex-col items-center justify-center gap-8 py-16">
 			<h2 className="text-4xl font-bold tracking-widest">Get in Touch</h2>
 			<Button title="BUTTON CTA" />
-		</div>
+		</section>
 	);
 };
 
