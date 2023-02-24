d757fa36d75491adceb1418c5bd8e9e2891d2cc6
jhigger <njohnjhigger@gmail.com>
2023-02-24 18:25:07 +0800

    edit

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -22,37 +22,59 @@ const Contact = () => {
 					className="mb-8 max-h-[720px] w-screen object-cover object-center"
 				/>
 
-				<div className="container mx-auto flex flex-col gap-8">
+				<div className="container mx-auto flex flex-col gap-8 last:mb-32">
 					<section className="flex flex-col gap-4">
-						<h2 className="mb-4 text-2xl uppercase text-primary">
+						<h2 className="mb-4 text-3xl uppercase text-primary">
 							Keep in touch
 						</h2>
-						<div className="grid grid-cols-1 gap-8 lg:w-10/12 lg:grid-cols-2 lg:self-end">
+						<div className="grid grid-cols-1 gap-8 tracking-widest lg:w-10/12 lg:grid-cols-2 lg:self-end">
 							<div className="flex flex-col gap-4">
-								<p>Palmerston North, New Zealand</p>
+								<h3 className="text-2xl font-bold">
+									Palmerston North,{" "}
+									<span className="text-gray-500">
+										New Zealand
+									</span>
+								</h3>
 								<p>
 									182 Broadway Avenue, Palmerston North
 									Central, Palmerston North 4410
 								</p>
 								<div className="flex flex-col lg:flex-row lg:gap-4">
-									<h3>Email:</h3>
-									<p>admin@duckettarchitecture.co.nz</p>
+									<h3 className="font-bold">Email:</h3>
+									<a
+										className="underline"
+										href="mailto: admin@duckettarchitecture.co.nz"
+									>
+										admin@duckettarchitecture.co.nz
+									</a>
 								</div>
 							</div>
-							<div className="flex flex-col gap-4">
+							<div className="flex flex-col gap-4 font-bold leading-10">
 								<div>
 									<h3>Call Us:</h3>
-									<p>06 356 735</p>
-									<p>+64 22 057 0360</p>
+									<p className="text-2xl">
+										06{" "}
+										<span className="text-gray-500">
+											356 735
+										</span>
+									</p>
+									<p className="text-2xl">
+										+64{" "}
+										<span className="text-gray-500">
+											22 057 0360
+										</span>
+									</p>
 								</div>
 
 								<div>
 									<h3>Work Hours</h3>
 									<div className="grid grid-cols-2">
 										<p>Monday - Friday</p>
-										<p>09:00 - 05:00</p>
+										<p className="font-normal">
+											09:00 - 05:00
+										</p>
 										<p>Saturday - Sunday</p>
-										<p>Closed</p>
+										<p className="font-normal">Closed</p>
 									</div>
 								</div>
 
@@ -69,7 +91,7 @@ const Contact = () => {
 					</section>
 
 					<section className="flex flex-col gap-4">
-						<h2 className="mb-4 text-2xl uppercase text-primary">
+						<h2 className="mb-4 text-3xl uppercase text-primary">
 							Work with us
 						</h2>
 						<div className="flex flex-col gap-2 lg:w-10/12 lg:self-end">
