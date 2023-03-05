9b45f6d3a588ca4f06184808045749332c77f82d
jhigger <njohnjhigger@gmail.com>
2023-03-06 06:19:19 +0800

    edit

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,7 +4,7 @@ type Props = { title: string; imageSrc: string };
 
 const HeroSection = ({ title, imageSrc }: Props) => {
 	return (
-		<div className="container relative mx-auto my-8">
+		<section className="container relative mx-auto my-8">
 			<div className="absolute top-1/2 left-1/2 mb-4 flex h-32 w-full -translate-x-1/2 -translate-y-1/2 items-center justify-center">
 				<div className="h-full w-full bg-black opacity-60"></div>
 				<h1 className="absolute text-center text-5xl font-bold tracking-widest text-primary">
@@ -17,7 +17,7 @@ const HeroSection = ({ title, imageSrc }: Props) => {
 				src={imageSrc}
 				className="mb-8 aspect-[9/16] w-full rounded-3xl object-cover object-center md:aspect-video"
 			/>
-		</div>
+		</section>
 	);
 };
 
