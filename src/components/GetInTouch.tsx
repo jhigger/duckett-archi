c41f0cacd72d991f4a12e6bcb668d8ce8802dfb4
jhigger <njohnjhigger@gmail.com>
2023-03-06 05:37:17 +0800

    add link

--- src/components/GetInTouch.tsx
+++ src/components/GetInTouch.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import Button from "./Button";
 
 const GetInTouch = () => {
 	return (
 		<section className="flex flex-col items-center justify-center gap-8 py-16">
 			<h2 className="text-4xl font-bold tracking-widest">Get in Touch</h2>
-			<Button title="BUTTON CTA" className="text-white" />
+			<Link href="/contact">
+				<Button title="Contact Us" className="text-white" />
+			</Link>
 		</section>
 	);
 };
