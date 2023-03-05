2a5831e4f1556baff10b58f9b5baf79ad34dd1ea
jhigger <njohnjhigger@gmail.com>
2023-03-06 05:33:30 +0800

    edit

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,7 +4,7 @@ type Props = { title: string; imageSrc: string };
 
 const HeroSection = ({ title, imageSrc }: Props) => {
 	return (
-		<div>
+		<div className="container mx-auto my-8">
 			<h1 className="mb-4 text-center text-4xl font-bold tracking-widest">
 				{title}
 			</h1>
@@ -14,7 +14,7 @@ const HeroSection = ({ title, imageSrc }: Props) => {
 				src={imageSrc}
 				width={1080}
 				height={720}
-				className="mb-8 max-h-[720px] w-screen rounded-3xl object-cover object-center"
+				className="mb-8 aspect-video w-full rounded-3xl object-cover object-center"
 			/>
 		</div>
 	);
