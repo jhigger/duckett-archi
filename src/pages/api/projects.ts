1c79323b6efb4eeee8514d9c566da58d4328c4da
jhigger <njohnjhigger@gmail.com>
2023-03-07 05:01:28 +0800

    change structure

--- src/pages/api/projects.ts
+++ src/pages/api/projects.ts
@@ -12,18 +12,18 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 		path.join(dirRelativeToPublicFolder, name)
 	);
 
-	const tree = categories.map((category) => {
-		if (category === undefined) return;
+	const tree = {};
+	categories.forEach((category) => {
 		const folderDir = path.join("./public", category);
 		const filenames = fs.readdirSync(folderDir);
 
 		const projectFolder = filenames.map((name) => {
 			const projectDir = path.resolve("./public", category, name);
 			const images = fs.readdirSync(projectDir);
-
 			return { [name]: images };
 		});
-		return { [path.basename(category)]: projectFolder };
+
+		Object.assign(tree, { [path.basename(category)]: projectFolder });
 	});
 
 	return res.status(200).json(tree);
