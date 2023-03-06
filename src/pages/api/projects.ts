32c385d84143e99432360ab000f83c263a13f170
jhigger <njohnjhigger@gmail.com>
2023-03-07 04:02:32 +0800

    add images

---
+++ src/pages/api/projects.ts
@@ -0,0 +1,30 @@
+import fs from "fs";
+import type { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+	const dirRelativeToPublicFolder = "projects";
+
+	const dir = path.resolve("./public", dirRelativeToPublicFolder);
+
+	const folderNames = fs.readdirSync(dir);
+	const categories = folderNames.map((name) =>
+		path.join(dirRelativeToPublicFolder, name)
+	);
+
+	const tree = categories.map((category) => {
+		if (category === undefined) return;
+		const folderDir = path.join("./public", category);
+		const filenames = fs.readdirSync(folderDir);
+
+		const projectFolder = filenames.map((name) => {
+			const projectDir = path.resolve("./public", category, name);
+			const images = fs.readdirSync(projectDir);
+
+			return { [name]: images };
+		});
+		return { [path.basename(category)]: projectFolder };
+	});
+
+	return res.status(200).json(tree);
+}
