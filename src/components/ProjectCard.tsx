0f87c9b1f07bfd77ce955c06c49e950c8caeb5fc
jhigger <njohnjhigger@gmail.com>
2023-03-07 08:57:49 +0800

    edit opacity

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -16,7 +16,8 @@ const ProjectCard = ({ src, title, link }: Props) => {
 				src={src}
 				className="h-[512px] w-[384px] object-cover object-center"
 			/>
-			<div className="absolute top-0 flex h-full w-full flex-col justify-end gap-4 bg-primary p-8 opacity-60 transition-all duration-300 ease-in-out group-hover:top-0 lg:top-[100%]">
+			<div className="absolute top-0 flex h-full w-full flex-col justify-end gap-4 bg-primary p-8 opacity-60 transition-all duration-300 ease-in-out group-hover:top-0 lg:top-[100%]"></div>
+			<div className="absolute top-0 flex h-full w-full flex-col justify-end gap-4 p-8 transition-all duration-300 ease-in-out group-hover:top-0 lg:top-[100%]">
 				<h3 className="text-4xl font-bold">{title}</h3>
 				<Link
 					href={link}
