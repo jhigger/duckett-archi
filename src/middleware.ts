4403b9a60251b5c9b9a428b5382210933759cffb
jhigger <njohnjhigger@gmail.com>
2023-03-10 11:07:16 +0800

    lowecase only some paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,10 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 const Middleware = (req: NextRequest) => {
-	if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase())
+	if (
+		req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase() ||
+		["_next", "projects"].includes(req.nextUrl.pathname.split("/")[1] ?? "")
+	)
 		return NextResponse.next();
 
 	return NextResponse.redirect(
