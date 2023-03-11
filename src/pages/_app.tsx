cbea2e0453204d1b11bba2d24cb025ea278b3e68
jhigger <njohnjhigger@gmail.com>
2023-03-11 14:24:33 +0800

    upgrade and add next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,24 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { Inter } from "next/font/google";
+import NextNProgress from "nextjs-progressbar";
 import Footer from "~/components/Footer";
 import Nav from "~/components/Nav";
-import NextNProgress from "nextjs-progressbar";
-
 import "~/styles/globals.css";
+
+const inter = Inter({
+	subsets: ["latin"],
+	weight: ["400", "700"],
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
 	return (
-		<div className="min-h-screen p-4 lg:p-8 ">
-			<NextNProgress color="#F6A01E" height={4} />
-
-			<Nav />
-
-			<Component {...pageProps} />
-
-			<Footer />
+		<div className={inter.className}>
+			<div className="min-h-screen p-4 lg:p-8">
+				<NextNProgress color="#F6A01E" height={4} />
+				<Nav />
+				<Component {...pageProps} />
+				<Footer />
+			</div>
 		</div>
 	);
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					<meta name="author" content="John Jhigger" />
+					<meta name="author" content="github.com/jhigger" />
 					<meta
 						name="description"
 						content="Duckett Architecture Palmerston North"
@@ -23,20 +23,6 @@ class MyDocument extends Document {
 					/>
 					<link rel="icon" href="/favicon.ico" />
 					<link rel="shortcut icon" href="/favicon.ico" />
-
-					<link
-						rel="preconnect"
-						href="https://fonts.googleapis.com"
-					/>
-					<link
-						rel="preconnect"
-						href="https://fonts.gstatic.com"
-						crossOrigin="anonymous"
-					/>
-					<link
-						href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
-						rel="stylesheet"
-					></link>
 				</Head>
 				<body>
 					<Main />
