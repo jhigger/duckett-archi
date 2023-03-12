5771bf077cd5c36d3f5bae86acbdb4b48fdf6d5f
jhigger <njohnjhigger@gmail.com>
2023-03-12 08:45:15 +0800

    edit mobile version

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -11,24 +11,39 @@ type Props = {
 const ProjectCard = ({ src, title, link }: Props) => {
 	return (
 		<div className="group relative h-min overflow-hidden rounded-3xl">
-			<Image
-				src={`/${src.replaceAll("\\", "/")}`}
-				alt="project"
-				width={640}
-				height={360}
-				quality={50}
-				loading="lazy"
-				className="h-[512px] w-[384px] object-cover object-center"
-			/>
-			<div className="absolute top-0 flex h-full w-full flex-col justify-end gap-4 bg-primary p-8 opacity-60 transition-all duration-300 ease-in-out group-hover:top-0 lg:top-[100%]"></div>
-			<div className="absolute top-0 flex h-full w-full flex-col justify-end gap-4 p-8 transition-all duration-300 ease-in-out group-hover:top-0 lg:top-[100%]">
-				<h3 className="text-4xl font-bold">{title}</h3>
-				<Link
-					href={link}
-					className="flex w-max items-center gap-4 self-end hover:text-white"
-				>
-					<p>See Project</p>
-					<BsChevronRight size={24} />
+			<div className="hidden lg:block">
+				<Image
+					src={`/${src.replaceAll("\\", "/")}`}
+					alt="project"
+					width={640}
+					height={360}
+					quality={50}
+					loading="lazy"
+					className="h-[512px] w-[384px] object-cover object-center"
+				/>
+				<div className="absolute top-0 flex h-full w-full flex-col justify-end gap-4 bg-primary p-8 opacity-60 transition-all duration-300 ease-in-out group-hover:top-0 lg:top-[100%]"></div>
+				<div className="absolute top-0 flex h-full w-full flex-col justify-end gap-4 p-8 transition-all duration-300 ease-in-out group-hover:top-0 lg:top-[100%]">
+					<h3 className="text-4xl font-bold">{title}</h3>
+					<Link
+						href={link}
+						className="flex w-max items-center gap-4 self-end hover:text-white"
+					>
+						<p>See Project</p>
+						<BsChevronRight size={24} />
+					</Link>
+				</div>
+			</div>
+			<div className="block lg:hidden">
+				<Link href={link}>
+					<Image
+						src={`/${src.replaceAll("\\", "/")}`}
+						alt="project"
+						width={640}
+						height={360}
+						quality={50}
+						loading="lazy"
+						className="h-[512px] w-[384px] object-cover object-center"
+					/>
 				</Link>
 			</div>
 		</div>
